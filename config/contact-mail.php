b44b1c00da03f59535cc200756c6541579ee91f9
peppuzzot <giuseppe.massaro2702@gmail.com>
2026-04-21 15:55:28 +0200

    refactr cofing/contact-mail.php

--- config/contact-mail.php
+++ config/contact-mail.php
@@ -5,15 +5,16 @@ return [
         'host' => 'smtps.aruba.it',
         'port' => 465,
         'encryption' => 'ssl',
-        'username' => 'info@wemarine.it',
-        'password' => 'wENDY2011',
+        'username' => 'info@tuodominio.it',
+        'password' => 'PASSWORD',
         'timeout' => 15,
     ],
     'mail' => [
-        'from_address' => 'info@wemarine.it',
+        'from_address' => 'info@tuodominio.it',
         'from_name' => 'WEMARINE',
-        'to_address' => 'info@wemarine.it',
+        'to_address' => 'info@tuodominio.it',
         'to_name' => 'WEMARINE',
         'subject_prefix' => 'Nuova richiesta dal sito',
     ],
 ];
+
